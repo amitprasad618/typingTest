18a0b911dea3103d0bec571a514878d57527c54b
amitprasad618 <amitprasad618@gmail.com>
2026-03-19 16:17:03 +0530

    move sitemap to api route

---
+++ src/api/sitemap.js
@@ -0,0 +1,34 @@
+export default function handler(req, res) {
+  res.setHeader("Content-Type", "application/xml");
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+    <url>
+      <loc>https://typing-test-one-orpin.vercel.app/</loc>
+    </url>
+
+    <url>
+      <loc>https://typing-test-one-orpin.vercel.app/typing-test</loc>
+    </url>
+
+    <url>
+      <loc>https://typing-test-one-orpin.vercel.app/typing-practice</loc>
+    </url>
+
+    <url>
+      <loc>https://typing-test-one-orpin.vercel.app/learn-typing</loc>
+    </url>
+
+    <url>
+      <loc>https://typing-test-one-orpin.vercel.app/improve-typing-speed</loc>
+    </url>
+
+    <url>
+      <loc>https://typing-test-one-orpin.vercel.app/wpm-calculator</loc>
+    </url>
+
+  </urlset>`;
+
+  res.status(200).send(sitemap);
+}
