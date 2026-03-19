e7832db118e64429d51363aa4d07a38040939f9b
amitprasad618 <amitprasad618@gmail.com>
2026-03-19 16:56:06 +0530

    switch to API route for sitemap to prevent 304 caching

--- src/api/sitemap.js
+++ src/api/sitemap.js
@@ -1,5 +1,13 @@
 export default function handler(req, res) {
-  res.setHeader("Content-Type", "application/xml");
+  // Set headers to prevent all caching and conditional requests
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate, proxy-revalidate, max-age=0');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  res.setHeader('Content-Type', 'application/xml');
+
+  // Remove headers that could cause conditional requests
+  res.removeHeader('ETag');
+  res.removeHeader('Last-Modified');
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
